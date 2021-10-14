6d492fadb40bb55583fb0cd7da61213f74da12a2
Ulric <ulric.debaert@outlook.fr>
2021-10-14 15:02:08 +0200

    Game Scripts and Scene

---
+++ Gamification_Proto/Assets/UD/UD_Scripts/UD_DataManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UD_DataManager : MonoBehaviour
+{
+    public float currentPoints;
+
+    public static string[] earningString;
+    public static string[] earningCodeString;
+
+    void Start()
+    {
+        currentPoints = PlayerPrefs.GetInt("totalPoint");
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Gamification_Proto/Assets/UD/UD_Scripts/UD_GameCommonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UD_GameCommonScript : MonoBehaviour
+{
+    public GameObject selfCanvas;
+
+    public bool thisGameIsActive;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+    public void StartGame()
+    {
+        thisGameIsActive = true;
+
+        selfCanvas.SetActive(true);
+    }
+
+    public void StopGame()
+    {
+        thisGameIsActive = false;
+
+        selfCanvas.SetActive(true);
+    }
+}
